0d295ffdc5ea32a346e292b31db0cb3d12c036c5
Santosh Kumar <kasantosh@yahoo.com>
2020-03-26 14:37:26 -0400

    Finished with the project

--- github.js
+++ github.js
@@ -2,18 +2,28 @@ class Github {
   constructor() {
     this.client_id = 'e3f5e3321b9d13df9c37';
     this.client_secret = '5730227e68fbcda1f69db1bf58de748ada704cf4';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc'
   }
+
+
 
   // Get user method
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+
     const profile = await profileResponse.json();
+
+    const repos = await reposResponse.json();
 
     // return profileData; returning as object below
     return {
-      profile
-      // same as profile: profile
+      profile,
+      repos
+      // same as profile: profile or repos: repos
     }
   }
 }
--- ui.js
+++ ui.js
@@ -29,7 +29,7 @@ class UI {
         </div>
       </div>
       <h3 class = "page-heading">Latest Repos</h3>
-      <div id = "repos></div>
+      <div id = "repos"></div>
     `;
 
   }
@@ -58,6 +58,29 @@ class UI {
     }, 3000);
   }
 
+  // Show user repos
+  showRepos(repos) {
+    let output = '';
+    repos.forEach(function (repo) {
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </div>
+                <div class="col-md-6">
+                <span class ="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+                <span class ="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+                <span class ="badge badge-success">Forks: ${repo.forks_count}</span>
+            </div>
+          </div>
+        </div>
+      `
+    });
+
+    // outpot repos
+    document.getElementById('repos').innerHTML = output;
+  }
 
   // Clear alert message
   clearAlert() {
